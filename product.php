8d3807608fbc8dc81db970cb83b48da46aa171b1
ansouth <ansouth@qq.com>
2018-05-10 11:00:31 +0800

    product
    product

--- product.php
+++ product.php
@@ -3,8 +3,227 @@ include 'header.php';
 ?>
 <div class="product">
 
-    Your html Code
-    5.公司产品
+   <div class="container">
+       <div class="product_item_left">
+               <br>
+               <div class="center-top">
+                   <span class="item1">产品分类</span>
+                   <span class="item2"></span>
+                   <span class="item3">PRODUCTS</span>
+               </div>
+               <br>
+               <ul class="center-list">
+                   <li><a href="#">
+                    <span>
+                        <span>豆腐美食系列</span>
+                        <strong></strong>
+                    </span>
+                       </a></li>
+                   <li><a href="#">
+                    <span>
+                        <span>商超系列</span>
+                        <strong></strong>
+                    </span>
+                       </a></li>
+                   <li><a href="#">
+                    <span>
+                        <span>水果系列</span>
+                        <strong></strong>
+                    </span>
+                       </a></li>
+                   <li><a href="#">
+                    <span>
+                        <span>土特产系列</span>
+                        <strong></strong>
+                    </span>
+                       </a></li>
+                   <li><a href="#">
+                    <span>
+                        <span>酸汤系列</span>
+                        <strong></strong>
+                    </span>
+                       </a></li>
+
+               </ul>
+               <br>
+
+               <br>
+               <div class="center-top">
+                   <span class="item1">新闻咨询</span>
+                   <span class="item2"></span>
+                   <span class="item3">NEWS</span>
+               </div>
+               <br>
+               <ul class="center-list">
+                   <li><a href="#">
+                    <span>
+                        <span>公司动态</span>
+                        <strong></strong>
+                    </span>
+                       </a></li>
+                   <li><a href="#">
+                    <span>
+                        <span>行业动态</span>
+                        <strong></strong>
+                    </span>
+                       </a></li>
+               </ul>
+               <ul class="company-contact">
+
+                   <br>
+                   <div class="center-top">
+                       <span class="item1">联系我们</span>
+                       <span class="item2"></span>
+                       <span class="item3">CONTACTUS</span>
+                   </div>
+                   <br>
+
+                   <h5>镇远乐豆坊食品有限公司</h5>
+                   <li>联系人:  <span></span></li>
+                   <li>座    机:<span></span></li>
+                   <li>传    真：<span></span></li>
+                   <li>手    机：<span></span></li>
+                   <li>Q     Q：<span></span></li>
+                   <li>邮    箱：<span></span></li>
+                   <li>地    址：<span></span></li>
+                   <li>网    址：<span></span></li>
+               </ul>
+       </div>
+       <div class="product_item_right">
+           <div class="item_right_bar">
+               <h2 class="contact_style_adr">联系方式与地址 </h2>
+               <div class="front_page"><a href="#">首页</a>
+                   <span>&gt;&gt;</span><a href="#">联系我们</a>
+                   <span>&gt;&gt;</span><a href="#">联系方式</a>
+               </div>
+           </div>
+           <div class="product_food_list">
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+
+                           <span><a href="">米豆腐</a></span>
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                       <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box clear_shadow">
+                   <ul>
+                       <li>
+                       <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box clear_shadow">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box clear_shadow">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+               <div class="product_food_list_box clear_shadow">
+                   <ul>
+                       <li>
+                           <div><img src="img/public_food_pic.png" alt=""></div>
+                           <span><a href="">米豆腐</a></span>
+
+                       </li>
+                   </ul>
+               </div>
+
+           </div>
+       </div>
+           <div class="center-paging clearfloat">
+               <span class="item"><a href="#">Prev</a></span>
+               <ul class="center-paging-list">
+                   <li><a class="itemss" href="#">1</a></li>
+                   <li><a href="#">2</a></li>
+                   <li><a href="#">3</a></li>
+               </ul>
+               <span class="item"><a href="#">Next</a></span>
+           </div>
+       <br>
+   </div>
 
 </div>
 <script>
