aabbee999cdedc27ca97e3f86ebf4820826dfe19
wangtong522323 <39079567+wangtong522323@users.noreply.github.com>
2018-05-11 12:31:36 +0800

    引入img
    引入img

--- index.php
+++ index.php
@@ -5,21 +5,22 @@ include 'header.php';
     <div class="about-us">
         <div class="container">
             <div class="left-img">
-                <img src="/img/about-model-img.png" alt="">
+                <img src="<?php bloginfo('template_url'); ?>/img/about-model-img.png" alt="">
             </div>
+
             <div class="right-characters">
                 <div class="center-top right-characters-top">
-                    <span class="item1">关于我们</span>
+                    <span class="item1"><?php ?>关于我们</span>
                     <span class="item2"></span>
                     <span class="item3">ABOUT US</span>
                 </div>
                 <p class="conten-p">贵州大学明德学院数字传媒系成立于2014年，设有数字媒体艺术、数字媒体艺术、艺术与科技、视觉传达与设计四个专业。围绕贵州大数据贵州创意文化产业，以泰豪创意产业集群为依托平台，以“互联网+文化/艺术/产品”为指导思想示范效应的数字艺术、技术人才培养高地干货黄金客户好几个号结构化即可国家法规环境法规和见过好几个环节赶紧回家都给他凡人歌人控股集团考虑过深刻了解公司考虑到该 我看过图片搜几个登记公司考虑到该。
                 </p>
-                <a href=""><img class="right-point" src="img/right-point.png" alt=""></a>
+                <a href=""><img class="right-point" src="<?php bloginfo('template_url'); ?>/img/right-point.png" alt=""></a>
             </div>
         </div>
         </div>
-
+    
 
     <div class="bottom container">
         <div class="list-left">
@@ -36,7 +37,7 @@ include 'header.php';
                     <p class="pde">隆先生是在攀枝花宏油庄冒菜店，经常吃冒菜的冒菜店，经常吃冒菜的隆先生是在攀枝花
                         宏油庄冒菜店，经常吃冒菜的冒菜店，经常吃 </p>
                     <span class="spanspan">
-                        <a href=""><img src="img/right-point.png" alt=""></a>
+                        <a href=""><img src="<?php bloginfo('template_url'); ?>/img/right-point.png" alt=""></a>
                         <p class="tigh-p"> 2016-04-12</p>
             </span>
                 </li>
@@ -47,7 +48,7 @@ include 'header.php';
                     <p class="pde">隆先生是在攀枝花宏油庄冒菜店，经常吃冒菜的冒菜店，经常吃冒菜的隆先生是在攀枝花
                         宏油庄冒菜店，经常吃冒菜的冒菜店，经常吃 </p>
                     <span class="spanspan">
-                       <a href=""><img src="img/right-point.png" alt=""></a>
+                       <a href=""><img <?php bloginfo('template_url'); ?>/src="img/right-point.png" alt=""></a>
                         <p class="tigh-p">2016-04-12</p>
             </span>
                 </li>
@@ -58,7 +59,7 @@ include 'header.php';
                     <p class="pde">隆先生是在攀枝花宏油庄冒菜店，经常吃冒菜的冒菜店，经常吃冒菜的隆先生是在攀枝花
                         宏油庄冒菜店，经常吃冒菜的冒菜店，经常吃 </p>
                     <span class="spanspan">
-                      <a href=""><img src="img/right-point.png" alt=""></a>
+                      <a href=""><img src="<?php bloginfo('template_url'); ?>/img/right-point.png" alt=""></a>
                         <p class="tigh-p"> 2016-04-12</p>
             </span>
                 </li>
@@ -67,13 +68,13 @@ include 'header.php';
         </div>
         <div class="right-list">
             <ul>
-                <li><img class="imgs1" src="img/list-right1.png" alt=""></li>
+                <li><img class="imgs1" src="<?php bloginfo('template_url'); ?>/img/list-right1.png" alt=""></li>
             </ul>
             <ul>
-                <li><img class="imgs2" src="img/list-right2.png" alt=""></li>
+                <li><img class="imgs2" src="<?php bloginfo('template_url'); ?>/img/list-right2.png" alt=""></li>
             </ul>
             <ul>
-                <li><img class="imgs3" src="img/list-right3.png" alt=""></li>
+                <li><img class="imgs3" src="<?php bloginfo('template_url'); ?>/img/list-right3.png" alt=""></li>
             </ul>
         </div>
     </div>
