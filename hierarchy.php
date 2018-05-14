ee692327c9812f8346956bea64e15e4984e67d91
LiangJunJie1 <LiangJunJie1@users.noreply.github.com>
2018-05-14 13:44:27 +0800

    1

--- hierarchy.php
+++ hierarchy.php
@@ -23,10 +23,7 @@ include 'header.php';
                     }
                     ?>
                 </ul>
-
-
-
-
+            </div>
             <div class="hierarchy-center-right">
                 <div class="center-top hierarchy-right-title">
                     <ul class="right-ul">
@@ -34,9 +31,9 @@ include 'header.php';
                             <a href="javascript:">首页<span><img src="<?php bloginfo('template_url'); ?>/img/about-right-pic.png" alt=""></span></a>
                         </li>
                         <li>
-                            <a href="javascript:">关于我们<span><img src="<?php bloginfo('template_url'); ?>/img/about-right-pic.png" alt=""></span></a>
+                            <a href="javascript:">溯源体系<span><img src="<?php bloginfo('template_url'); ?>/img/about-right-pic.png" alt=""></span></a>
                         </li>
-                        <li><a href="javascript:">展示公司介绍</a></li>
+                        <li><a href="javascript:">原产料基地</a></li>
                     </ul>
                 </div>
                 <div class="right-top1">
@@ -56,11 +53,34 @@ include 'header.php';
                         </span>
                     </div>
                 </div>
-
-                <div class="hierarchy-new-center"></div>
-
-
-
+                <div class="picture1"></div>
+                <div>
+                    <p>
+                        公司成立于2015年，注册资金800万元，加工厂房3000多平方米，办公楼及研发
+                        中心1200平方米，现有员工36人。注册商标为“潘小小”，现有发明专利11件，
+                        《传统青溪豆腐旅游休闲食品研究与产业化生产》通过黔东南州科技成果鉴定，
+                        为贵州省科技型企业，黔东南州农业产业化经营重点龙头企业，获得SC认证。公司在
+                        保留青溪豆腐特有香味的基础上，规模化生产青溪传统特色豆制品：青溪豆腐、恋爱豆
+                        腐、游浆秘制豆腐，酱香豆腐、香蹄豆腐以及豆干旅游休闲食品等豆腐系列产品，
+                        公司旗下有综合养殖基地、果树种植基地，很好地解决了豆渣等生产下脚料的综合
+                        利用，形成了加、养、种循环利用的立体农业产业链。都会感受到韩国发的规
+                        划都是借口借口。
+                    </p>
+                </div>
+                <div class="picture2"></div>
+                <div><p>   公司现有在职员工36人，行政   公司现有在职员工36人，行政管理层：总经理、销售
+                        副总经理、生产副总经理、会计、出纳等5人；销售人员8人；生产工人员23人。大专以上
+                        学历人员15人，其中从事食品加工专业技术人员4人。公司现设办公室、生产部、供应部、
+                        销售部、质管部、技术研发中心等6个部门，建立了比较健全的行政、人事、财务等内控管
+                        理制度，人员配备齐全，专业结构合理，能够确保公司有效运行，发展壮大。</p>
+                    <p>管理层：总经理、销售副总经理、生产副总经理、会计、出纳等5人；销售人员8人；
+                    生产工人员23人。大专以上学历人员15人，其中从事食品加工专业技术人员4人。公司现设办公
+                    室、生产部、供应部、销售部、质管部、技术研发中心等6个部门，建立了比较健全的行政、
+                    人事、财务等内控管理制度，人员配备齐全，专业结构合理，能够确保公司有效运行，发展壮
+                    大。欧艾斯发货单ID福is豆腐干ID港囧速度快关键是看到搜到故事哦的感觉好哦如果iOS的井冈
+                        山大学瓯双方都贵哦双方的感觉舒服的非事故就偶是发的。</p>
+                </div>
+                <div class="picture3"></div>
             </div><!--about-center-right 结束-->
         </div>
     </div><!--container 结束-->
